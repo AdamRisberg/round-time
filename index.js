d66ea42f6fde83a1e109545ba8aad3c13e0c46b5
Adam Risberg <myniztan@hotmail.com>
2020-04-14 01:11:00 -0500

    Add index for exporting functions

---
+++ index.js
@@ -0,0 +1,14 @@
+const roundDate = require("./roundDate");
+const { createRoundingFunc, TIME_UNITS, MODE } = roundDate;
+
+module.exports = {
+  hoursUp: createRoundingFunc(TIME_UNITS.HOURS, MODE.UP),
+  hoursDown: createRoundingFunc(TIME_UNITS.HOURS, MODE.DOWN),
+  hoursNearest: createRoundingFunc(TIME_UNITS.HOURS, MODE.NEAREST),
+  minutesUp: createRoundingFunc(TIME_UNITS.MINUTES, MODE.UP),
+  minutesDown: createRoundingFunc(TIME_UNITS.MINUTES, MODE.DOWN),
+  minutesNearest: createRoundingFunc(TIME_UNITS.MINUTES, MODE.NEAREST),
+  secondsUp: createRoundingFunc(TIME_UNITS.SECONDS, MODE.UP),
+  secondsDown: createRoundingFunc(TIME_UNITS.SECONDS, MODE.DOWN),
+  secondsNearest: createRoundingFunc(TIME_UNITS.SECONDS, MODE.NEAREST),
+};
